afc0ff07a825c93717297de7ee82b1147e4c9b60
omaradwanyasin <oyomaryasin@gmail.com>
2024-06-04 21:29:30 +0300

    all api working including notification

--- Services/Hubs/NotificationHub.cs
+++ Services/Hubs/NotificationHub.cs
@@ -3,8 +3,23 @@ using System.Threading.Tasks;
 
 public class NotificationHub : Hub
 {
-    public async Task SendNotification(string message)
+    public async Task SendNotification(string message, string severity, string userId)
     {
-        await Clients.All.SendAsync("ReceiveNotification", message);
+        await Clients.Group(userId).SendAsync("ReceiveNotification", message, severity);
+    }
+
+
+    public override async Task OnConnectedAsync()
+    {
+        var userId = Context.GetHttpContext().Request.Query["userId"];
+        await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        var userId = Context.GetHttpContext().Request.Query["userId"];
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, userId);
+        await base.OnDisconnectedAsync(exception);
     }
 }
